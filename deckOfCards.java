f6aa31bd2795cae6beb70a88961a5e8b80b97129
Yavik Kapadia <yavikkapadia@gmail.com>
2021-11-13 16:32:26 -0800

    Added constructors
    Added constructors and copy constructors.

--- deckOfCards.java
+++ deckOfCards.java
@@ -20,6 +20,38 @@ class Card
    private char value;
    private Suit suit;
    private boolean errorFlag;
+    /**
+    * constructor for Card object
+    *
+    * @param value of the card
+    * @param suit  of the card
+    */
+   public Card(char value, Suit suit)
+   {
+      this.value = value;
+      this.suit = suit;
+   }
+
+   /**
+    * constructor with default values
+    */
+   public Card()
+   {
+      this.value = 'A';
+      this.suit = Suit.Spades;
+   }
+
+   /**
+    * a copy constructor that will create a duplicate of the original
+    * which is not a reference
+    *
+    * @param origCard original card object
+    */
+   public Card(Card origCard)
+   {
+      this.value = origCard.getValue();
+      this.suit = origCard.getSuit();
+   }
 
 
    //stringizer
