34f45ba6a46c74b8b54043d48f257df0c12bef8b
Nix Melos <yavikkapadia@gmail.com>
2021-11-26 11:07:04 -0800

    completed phase 1

---
+++ src/CardDisplayer.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CardDisplayer
+{
+   static final int NUM_CARD_IMAGES = 57;
+   static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
+
+   /**
+    *
+    */
+   static void loadCardIcons()
+   {
+      String dirPrefix = "images/";
+      String ext = ".gif";
+      int i = 0;
+      for (int val = 0; val < 14; val++)
+      {
+         for (int suit = 0; suit < 4; suit++)
+         {
+            String cardVal = intToCardValue(val);
+            String cardSuit = intToCardSuit(suit);
+            String fileName = dirPrefix + cardVal + cardSuit + ext;
+            icon[i] = new ImageIcon(fileName);
+            i++;
+         }
+      }
+      icon[NUM_CARD_IMAGES-1] = new ImageIcon(dirPrefix + "BK" + ext);
+
+   }
+
+   /**
+    * helper method that returns corresponding card value
+    *
+    * @param k index of card value in array
+    * @return card value
+    */
+   static String intToCardValue(int k)
+   {
+      char[] cardVal = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J',
+            'Q', 'K', 'X' };
+
+      if (k < 0 || k > cardVal.length - 1)
+      {
+         return "invalid value";
+      }
+      else
+      {
+         return Character.toString(cardVal[k]);
+      }
+
+   }
+
+   /**
+    * helper method that returns corresponding card suit
+    *
+    * @param k index of suit in array
+    * @return suit value
+    */
+   static String intToCardSuit(int k)
+   {
+      char[] suitVal = { 'C', 'D', 'H', 'S' };
+
+      if (k < 0 || k > suitVal.length)
+      {
+         return "invalid value";
+      }
+      else
+      {
+         return Character.toString(suitVal[k]);
+      }
+   }
+
+   public static void main(String[] args)
+   {
+      int k;
+
+      // prepare the image icon array
+      loadCardIcons();
+
+      // establish main frame in which program will run
+      JFrame frmMyWindow = new JFrame("Card Room");
+      frmMyWindow.setSize(1150, 650);
+      frmMyWindow.setLocationRelativeTo(null);
+      frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // set up layout which will control placement of buttons, etc.
+      FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);
+      frmMyWindow.setLayout(layout);
+
+      // prepare the image label array
+      JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         labels[k] = new JLabel(icon[k]);
+
+      // place your 3 controls into frame
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         frmMyWindow.add(labels[k]);
+
+      // show everything to the user
+      frmMyWindow.setVisible(true);
+   }
+
+}
---
+++ src/CardTable.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class CardTable extends JFrame
+{
+   static int MAX_CARDS_PER_HAND = 56;
+   static int MAX_PLAYERS = 2;
+   private int numCardsPerHand;
+   private int numPlayers;
+   public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+
+
+}
