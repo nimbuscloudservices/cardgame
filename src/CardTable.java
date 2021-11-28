916c08d8673d0b29b8b85723fc1e017bc7078176
Saul <saul.mendoza1995@gmail.com>
2021-11-27 18:52:49 -0800

    Update CardTable.java

--- src/CardTable.java
+++ src/CardTable.java
@@ -1,4 +1,6 @@
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class CardTable extends JFrame
 {
@@ -8,5 +10,73 @@ public class CardTable extends JFrame
    private int numPlayers;
    public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
 
+   public CardTable(String title, int numCardsPerHand, int numPlayers)
+   {
+      super(title);
+      setLayout(new BorderLayout(1, 3));
+      JLabel pnlComputerHand = new JLabel( "Computer Hand");
+      add(pnlComputerHand, BorderLayout.NORTH);
 
+      JLabel pnlPlayArea = new JLabel( "Playing Area");
+      add(pnlPlayArea, BorderLayout.CENTER);
+
+      JLabel pnlHumanHand = new JLabel( "Your Hand");
+      add(pnlHumanHand, BorderLayout.SOUTH);
+
+      if(numCardsPerHand > CardTable.MAX_CARDS_PER_HAND && numPlayers > CardTable.MAX_PLAYERS)
+      {
+         this.numCardsPerHand = CardTable.MAX_CARDS_PER_HAND;
+         this.numPlayers = CardTable.MAX_PLAYERS;
+      }
+      else if(numCardsPerHand > CardTable.MAX_CARDS_PER_HAND && numPlayers <= CardTable.MAX_PLAYERS)
+      {
+         this.numCardsPerHand = CardTable.MAX_CARDS_PER_HAND;
+         this.numPlayers = numPlayers;
+      }
+      else if(numCardsPerHand <= CardTable.MAX_CARDS_PER_HAND && numPlayers > CardTable.MAX_PLAYERS)
+      {
+         this.numCardsPerHand = numCardsPerHand;
+         this.numPlayers = CardTable.MAX_PLAYERS;
+      }
+      else
+      {
+         this.numCardsPerHand = numCardsPerHand;
+         this.numPlayers = numPlayers;
+      }
+   }
+   
+   static int NUM_CARDS_PER_HAND = 7;
+   static int  NUM_PLAYERS = 2;
+   static JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
+   static JLabel[] humanLabels = new JLabel[NUM_CARDS_PER_HAND];
+   static JLabel[] playedCardLabels  = new JLabel[NUM_PLAYERS];
+   static JLabel[] playLabelText  = new JLabel[NUM_PLAYERS];
+
+   public static void main(String[] args)
+   {
+      int k;
+      Icon tempIcon;
+
+      // establish main frame in which program will run
+      CardTable myCardTable
+         = new CardTable("CardTable", NUM_CARDS_PER_HAND, NUM_PLAYERS);
+      myCardTable.setSize(800, 600);
+      myCardTable.setLocationRelativeTo(null);
+      myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // show everything to the user
+      myCardTable.setVisible(true);
+
+      // CREATE LABELS ----------------------------------------------------
+      //code goes here ...
+
+      // ADD LABELS TO PANELS -----------------------------------------
+      //code goes here ...
+
+      // and two random cards in the play region (simulating a computer/hum ply)
+      //code goes here ...
+
+      // show everything to the user
+      myCardTable.setVisible(true);
+   }
 }
