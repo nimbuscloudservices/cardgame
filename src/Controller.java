caa534e0d7731d3afe19f78f3cb212beeecea808
Saul <saul.mendoza1995@gmail.com>
2021-12-07 20:48:16 -0800

    started working on game

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,8 @@
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class Controller implements ActionListener
 {
@@ -33,17 +33,27 @@ public class Controller implements ActionListener
    public void initialize()
    {
       theView.initializeButtons(this);
+      
       theView.setIconButtons(theView.button1, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(0)));
+
       theView.setIconButtons(theView.button2, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(1)));
+
       theView.setIconButtons(theView.button3, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(2)));
+  
       theView.setIconButtons(theView.button4, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(3)));
+
       theView.setIconButtons(theView.button5, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(4)));
+
       theView.setIconButtons(theView.button6, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(5)));
+
       theView.setIconButtons(theView.button7, theModel.initializeHumanCards(SuitMatchGame.getHand(1).inspectCard(6)));
+  
       for(int i=0; i<7; i++)
       {
         theView.initializeComputerCard(theModel.initializeComputerCards(), i);
       }
+      
+      theView.initializePlayArea(theModel.getHumanCard(theModel.firstCardInStack(SuitMatchGame.getCardFromDeck())), theModel.getHumanCard(theModel.firstCardInStack(SuitMatchGame.getCardFromDeck())), theModel.getHumanCard(theModel.firstCardInStack(SuitMatchGame.getCardFromDeck())));
    }
    public void checkDeck(JButton button, int index)
    {
@@ -68,9 +78,30 @@ public class Controller implements ActionListener
          theView.removeComputerCard(index);
       }
    }
+   public void addCardToStack(String numberOfDeck, int player, int indexOf)
+   {
+      if(numberOfDeck == "First")
+      {
+         theView.addCardToStack(1,theModel.getHumanCard(SuitMatchGame.playCard(player,indexOf)));
+      }
+      else if (numberOfDeck == "Second")
+      {
+         theView.addCardToStack(2,theModel.getHumanCard(SuitMatchGame.playCard(player,indexOf)));
+      }
+      else if (numberOfDeck == "Third")
+      {
+         theView.addCardToStack(3,theModel.getHumanCard(SuitMatchGame.playCard(player,indexOf)));
+      }
+      else
+      {
+         theView.displayCP(1, theModel.cannotPlayCounter(1));
+      }
+      
+   }
    @Override
    public void actionPerformed(ActionEvent e)
    {
+
       String actionCommand = e.getActionCommand();
 
       Random ran = new Random();
@@ -79,56 +110,67 @@ public class Controller implements ActionListener
 
       if (actionCommand.equals("1"))
       {
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,0));
          
-         theView.setPlayArea(theView.button1, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(0),
-               SuitMatchGame.getHand(0).inspectCard(i)));   
+         addCardToStack(answer, 1, 0);
+         
          checkDeck(theView.button1, i);
       }
 
       else if (actionCommand.equals("2"))
       {
-         theView.setPlayArea(theView.button2, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(1),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,1));
+         
+         addCardToStack(answer, 1, 1);
+         
          checkDeck(theView.button2, i);
       }
       else if (actionCommand.equals("3"))
       {
-         theView.setPlayArea(theView.button3, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(2),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,2));
+         
+         addCardToStack(answer, 1, 2);
+         
          checkDeck(theView.button3, i);
       }
       else if (actionCommand.equals("4"))
       {
-         theView.setPlayArea(theView.button4, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(3),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,3));
+         
+         addCardToStack(answer, 1, 3);
+         
          checkDeck(theView.button4, i);
       }
       else if (actionCommand.equals("5"))
       {
-         theView.setPlayArea(theView.button5, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(4),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,4));
+         
+         addCardToStack(answer, 1, 4);
+         
          checkDeck(theView.button5, i);
       }
       else if (actionCommand.equals("6"))
       {
-         theView.setPlayArea(theView.button6, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(5),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,5));
+         
+         addCardToStack(answer, 1, 5);
          checkDeck(theView.button6, i);
       }
       else if (actionCommand.equals("7"))
       {
-         theView.setPlayArea(theView.button7, theModel.getComputerCard(SuitMatchGame.getHand(0).inspectCard(i)));
+
+         String answer = theModel.playGame(SuitMatchGame.playCard(1,6));
          
-         theView.displayRoundWinner(theModel.playGame(SuitMatchGame.getHand(1).inspectCard(6),
-               SuitMatchGame.getHand(0).inspectCard(i)));
+         addCardToStack(answer, 1, 6);
+         
          checkDeck(theView.button7, i);
       }
+      else if(actionCommand.equals("Cannot Play"))
+      {
+         theView.displayCP(1, theModel.cannotPlayCounter(1));
+      }
+      
       //check if hands for each player are empty
       if(theView.emptyComputerHands()== true)
       {
@@ -140,5 +182,191 @@ public class Controller implements ActionListener
       }
       
    }
+   /**
+    * Timer class is the first half of the Timer, this class handles the
+    * multithreading aspect of the timer.
+    */
+   public static class Timer extends Thread
+   {
+      private String currentTime;
+      private int seconds = 0;
+      private boolean threadRunning = true;
 
+      /**
+       * default constructor that calls the constructor for Thread
+       */
+      public Timer()
+      {
+         super();
+      }
+
+      /**
+       * Constructor with specified time, used for paused timer.
+       * @param timeStartValue
+       */
+      public Timer(int timeStartValue)
+      {
+         this.seconds = timeStartValue - 1;
+      }
+      //run() contains all of needed timer code.
+      @Override
+      public void run()
+      {
+         while(isThreadRunning())
+         {
+            if(this.seconds < 6000)
+            {
+               this.seconds += 1;
+            }
+            else
+            {
+               this.seconds = 0;
+            }
+            View.autoTimer.setText(this.getFormattedTime(getSeconds()));
+            doNothing((long) 1000);
+         }
+      }
+
+      /**
+       * Terminates thread loop running the run() method
+       * @return true if successful
+       */
+      public boolean killThread()
+      {
+         this.threadRunning = false;
+         return true;
+      }
+
+      /**
+       * gets status of thread
+       * @return true if running false if not
+       */
+      public boolean isThreadRunning()
+      {
+         return threadRunning;
+      }
+
+      /**
+       * sets threadrunning status
+       * @param threadRunning
+       */
+      public void setThreadRunning(boolean threadRunning)
+      {
+         this.threadRunning = threadRunning;
+      }
+
+      /**
+       * gets the number of seconds elapsed since timer was started.
+       * @return seconds elapsed
+       */
+      public int getSeconds()
+      {
+         return seconds;
+      }
+
+      /**
+       * sets seconds elapsed, used for resetting time.
+       * @param seconds to set
+       */
+      public void setSeconds(int seconds)
+      {
+         this.seconds = seconds;
+      }
+
+      /**
+       * formats and returns a string that is used for timer's Jlabel.
+       * Uses MM:ss format
+       * @param totalSeconds seconds
+       * @return formatted string in MM:ss format
+       */
+      private String getFormattedTime( int totalSeconds)
+      {
+         int hours = totalSeconds / 3600;
+         int minutes = (totalSeconds % 3600) / 60;
+         int seconds = totalSeconds % 60;
+         String timeText = String.format("%02d:%02d:%02d", hours, minutes,
+               seconds);
+         return timeText;
+      }
+
+      /**
+       * Allows thread to sleep for a number of milliseconds and used for
+       * keeping time synchronized.
+       * @param milliseconds to donothing
+       */
+      private void doNothing(long milliseconds)
+      {
+         try
+         {
+            Thread.sleep(milliseconds);
+         }
+         catch (InterruptedException e)
+         {
+            System.out.println("Unexpected interrupt");
+            System.exit(0);
+         }
+      }
+
+      public String getCurrentTime()
+      {
+         return getFormattedTime(this.seconds);
+      }
+
+      public void setCurrentTime(int seconds)
+      {
+         this.currentTime = getFormattedTime(seconds);
+      }
+   }
+   /**
+    * TimerView class is the second half of the timer.
+    * This class is used as a JLabel
+    */
+   public static class TimerView extends JLabel implements ActionListener
+   {
+      private JButton timerBtn =  new JButton();
+      private Timer timerThread = new Timer();
+
+      public TimerView()
+      {
+         timerBtn.addActionListener(this);
+         this.setHorizontalAlignment(SwingConstants.CENTER);
+         setText("00:00:00");
+         setFont(new Font("Serif", Font.BOLD, 20));
+      }
+      public TimerView(boolean startTimer)
+      {
+         this();
+         if(startTimer)
+         {
+            timerThread.start();
+         }
+      }
+
+      public JButton timerToggle()
+      {
+         return timerBtn;
+      }
+      public boolean rstTimer()
+      {
+         this.timerThread.setSeconds(0);
+         return true;
+      }
+      /**
+       * Invoked when an action occurs.
+       *
+       * @param e the event to be processed
+       */
+      @Override public void actionPerformed(ActionEvent e)
+      {
+         if(timerThread.isAlive())
+         {
+            timerThread.killThread();
+            timerThread = new Timer(timerThread.getSeconds());
+         }
+         else
+         {
+            timerThread.start();
+         }
+      }
+   }
 }
